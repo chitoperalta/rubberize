80bd4bd2a125ae6e34cfcc025920a4df55d253cd
Chito Peralta <chitoangeloperalta@gmail.com>
2025-03-28 21:02:42 +0800

    fix: expose ExprLatex and StmtLatex

--- rubberize/latexer/__init__.py
+++ rubberize/latexer/__init__.py
@@ -5,6 +5,8 @@ Python code source string.
 from rubberize.latexer.latexer import latexer
 from rubberize.latexer.components.calcsheet import CalcSheet
 from rubberize.latexer.components.table import Table
+from rubberize.latexer.expr_latex import ExprLatex
+from rubberize.latexer.stmt_latex import StmtLatex
 
 from rubberize.latexer.calls import register_call_converter
 from rubberize.latexer.objects import register_object_converter
