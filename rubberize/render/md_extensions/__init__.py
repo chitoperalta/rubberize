d01c561cdb63813ab7ef629895d44af4c549584f
Chito Peralta <chitoangeloperalta@gmail.com>
2026-03-12 05:13:49 +0800

    fix(render): reactivate inline rubberize

--- rubberize/render/md_extensions/__init__.py
+++ rubberize/render/md_extensions/__init__.py
@@ -1,6 +1,6 @@
 """Custom markdown extensions."""
 
 from rubberize.render.md_extensions.alert import Alert
-from rubberize.render.md_extensions.irbz import InlineRubberize
+from rubberize.render.md_extensions.inline_rubberize import InlineRubberize
 from rubberize.render.md_extensions.latex_linebreak import LatexLinebreak
 from rubberize.render.md_extensions.small import Small
--- rubberize/render/md_extensions/inline_rubberize.py
+++ rubberize/render/md_extensions/inline_rubberize.py
@@ -22,13 +22,12 @@ class InlineRubberizeProcessor(InlineProcessor):
         self, pattern, md=None, namespace: Optional[dict[str, Any]] = None
     ):
         super().__init__(pattern, md)
-        self.namespace = namespace
+        self.ns = namespace
 
     def handleMatch(self, m, data):
         text = m.group(1).strip()
         text_ast = ast_c.parse(text)
-        text_latex: StmtLatex = ModVisitor(self.namespace).visit(text_ast)[0]
-        text_latex = StmtLatex(None, None)
+        text_latex: StmtLatex = ModVisitor(self.ns).visit(text_ast)[0]
 
         text_str = ""
         if text_latex.latex:
@@ -45,12 +44,12 @@ class InlineRubberize(Extension):
 
     def __init__(self, namespace: Optional[dict[str, Any]] = None, **kwargs):
         super().__init__(**kwargs)
-        self.namespace = namespace
+        self.ns = namespace
 
     def extendMarkdown(self, md):
         pattern = r"(?<!\\)\{\{\s*(.*?)\s*\}\}"
         md.inlinePatterns.register(
-            InlineRubberizeProcessor(pattern, md, self.namespace),
+            InlineRubberizeProcessor(pattern, md, self.ns),
             "python_expression",
             175,
         )
